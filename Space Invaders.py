35f88d666b55845a08e4f56fa03539fdac5b1935
rishavnathpati <patirishavnath@gmail.com>
2021-02-23 23:41:28 +0530

    Pong game

---
+++ Space Invaders.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+import random
+import time
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+
